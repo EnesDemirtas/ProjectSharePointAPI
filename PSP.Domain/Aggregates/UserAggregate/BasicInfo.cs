6b5f107300adbbf2db4d8650bf6e3bf02c246ed8
EnesDemirtas <enesdemirtas255@gmail.com>
2022-05-11 14:12:27 +0300

    feat: add user aggregate

---
+++ PSP.Domain/Aggregates/UserAggregate/BasicInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSP.Domain.Aggregates.UserAggregate {
+
+    public class BasicInfo {
+
+        private BasicInfo() {
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string UserName { get; private set; }
+        public string EmailAddress { get; private set; }
+
+        public static BasicInfo CreateBasicInfo(string firstName, string lastName, string userName, string email) {
+            var validator = new BasicInfoValidator();
+
+            var objToValidate = new BasicInfo {
+                FirstName = firstName,
+                LastName = lastName,
+                UserName = userName,
+                EmailAddress = email
+            };
+
+            var validationResult = validator.Validate(objToValidate);
+
+            if (validationResult.IsValid) return objToValidate;
+
+            var exception = new UserProfileNotValidException("The user profile is not valid");
+            foreach (var error in validationResult.Errors) {
+                exception.ValidationErrors.Add(error.ErrorMessage);
+            }
+
+            throw exception;
+        }
+    }
+}
---
+++ PSP.Domain/Aggregates/UserAggregate/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSP.Domain.Aggregates.UserAggregate {
+
+    public class User {
+
+        private User() {
+        }
+
+        public Guid UserId { get; private set; }
+        public string IdentityId { get; private set; }
+        public BasicInfo BasicInfo { get; private set; }
+
+        // Factory Method
+        public static User CreateUser(string identityId, BasicInfo basicInfo) {
+            return new User {
+                IdentityId = identityId,
+                BasicInfo = basicInfo
+            };
+        }
+
+        // public methods
+        public void UpdateBasicInfo(BasicInfo newInfo) {
+            BasicInfo = newInfo;
+        }
+    }
+}
