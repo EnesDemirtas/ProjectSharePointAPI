e5d12973c29a1506987f5cc38bf4efcbc4b9668b
EnesDemirtas <enesdemirtas255@gmail.com>
2022-05-11 15:31:51 +0300

    feat: add jwt and swagger options

---
+++ PSP.Api/Options/ConfigureSwaggerOptions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace PSP.Api.Options {
+
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions> {
+
+        public ConfigureSwaggerOptions() {
+        }
+
+        public void Configure(SwaggerGenOptions options) {
+            var scheme = GetJwtSecurityScheme();
+            options.AddSecurityDefinition(scheme.Reference.Id, scheme);
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement{
+                {scheme, new string[0] }
+            });
+        }
+
+        private OpenApiSecurityScheme GetJwtSecurityScheme() {
+            return new OpenApiSecurityScheme {
+                Name = "JWT Authentication",
+                Description = "Provide a JWT Bearer",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                Reference = new OpenApiReference {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+        }
+    }
+}
