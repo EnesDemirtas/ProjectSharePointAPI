34ea629aa1aa32eb5151f094947d86e9f2a5d6c1
EnesDemirtas <enesdemirtas255@gmail.com>
2022-05-11 15:31:19 +0300

    feat: add data context

---
+++ PSP.Dal/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PSP.Domain.Aggregates.ProjectAggregate;
+using PSP.Domain.Aggregates.UserAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSP.Dal {
+
+    public class DataContext : IdentityDbContext {
+
+        public DataContext(DbContextOptions options) : base(options) {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        }
+    }
+}
