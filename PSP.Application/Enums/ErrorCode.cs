8a62f7b12dd54387ad5f06a80790544f0ec1759a
EnesDemirtas <enesdemirtas255@gmail.com>
2022-05-12 15:34:31 +0300

    feat: add standard error and result model

---
+++ PSP.Application/Enums/ErrorCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSP.Application.Enums {
+
+    public enum ErrorCode {
+        NotFound = 404,
+        ServerError = 500,
+
+        // Valdiation errors should be in the range 100 - 199
+        ValidationError = 101,
+
+        // Infrastructure errors should be in the range 200 - 299
+        IdentityUserAlreadyExists = 201,
+
+        IdentityCreationFailed = 202,
+
+        UnknownError = 999
+    }
+}
---
+++ PSP.Application/Models/Error.cs
@@ -0,0 +1,14 @@
+﻿using PSP.Application.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSP.Application.Models {
+
+    public class Error {
+        public ErrorCode Code { get; set; }
+        public string Message { get; set; }
+    }
+}
