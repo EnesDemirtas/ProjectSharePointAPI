42f5550db6fe5c029b892fa8ef9e58dd39494194
EnesDemirtas <enesdemirtas255@gmail.com>
2022-05-12 15:23:13 +0300

    edit: project config files are changed

--- PSP.Api/Options/ConfigureSwaggerOptions.cs
+++ PSP.Api/Options/ConfigureSwaggerOptions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -6,16 +7,34 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 namespace PSP.Api.Options {
 
     public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions> {
+        private readonly IApiVersionDescriptionProvider _provider;
 
-        public ConfigureSwaggerOptions() {
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) {
+            _provider = provider;
         }
 
         public void Configure(SwaggerGenOptions options) {
+            foreach (var description in _provider.ApiVersionDescriptions) {
+                options.SwaggerDoc(description.GroupName, CreateVersionInfo(description));
+            }
             var scheme = GetJwtSecurityScheme();
             options.AddSecurityDefinition(scheme.Reference.Id, scheme);
             options.AddSecurityRequirement(new OpenApiSecurityRequirement{
                 {scheme, new string[0] }
             });
+        }
+
+        private OpenApiInfo CreateVersionInfo(ApiVersionDescription description) {
+            var info = new OpenApiInfo {
+                Title = "ProjectSharePointAPI",
+                Version = description.ApiVersion.ToString()
+            };
+
+            if (description.IsDeprecated) {
+                info.Description = "This API version has been deprecated";
+            }
+
+            return info;
         }
 
         private OpenApiSecurityScheme GetJwtSecurityScheme() {
---
+++ PSP.Application/Users/Commands/UpdateUserBasicInfoCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using PSP.Application.Models;
+using PSP.Domain.Aggregates.UserAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSP.Application.Users.Commands {
+
+    public class UpdateUserBasicInfoCommand : IRequest<OperationResult<UserProfile>> {
+        public Guid UserProfileId { get; set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string UserName { get; private set; }
+        public string EmailAddress { get; private set; }
+    }
+}
