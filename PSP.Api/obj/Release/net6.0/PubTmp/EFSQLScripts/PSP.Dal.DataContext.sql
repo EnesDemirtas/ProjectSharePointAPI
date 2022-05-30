6aadd70f90249389904fdff21d8a1fedd1123623
EnesDemirtas <enesdemirtas255@gmail.com>
2022-05-31 02:19:25 +0300

    unnecessary: file changes

--- PSP.Api/obj/Release/net6.0/PubTmp/EFSQLScripts/PSP.Dal.DataContext.sql
+++ PSP.Api/obj/Release/net6.0/PubTmp/EFSQLScripts/PSP.Dal.DataContext.sql
@@ -287,3 +287,162 @@ GO
 COMMIT;
 GO
 
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530142019_add_category')
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Projects]') AND [c].[name] = N'Category');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Projects] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [Projects] DROP COLUMN [Category];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530142019_add_category')
+BEGIN
+    CREATE TABLE [Categories] (
+        [CategoryId] uniqueidentifier NOT NULL,
+        [CategoryName] nvarchar(max) NOT NULL,
+        [CategoryDescription] nvarchar(max) NOT NULL,
+        [CreatedDate] datetime2 NOT NULL,
+        [LastModified] datetime2 NOT NULL,
+        [ProjectId] uniqueidentifier NULL,
+        CONSTRAINT [PK_Categories] PRIMARY KEY ([CategoryId]),
+        CONSTRAINT [FK_Categories_Projects_ProjectId] FOREIGN KEY ([ProjectId]) REFERENCES [Projects] ([ProjectId])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530142019_add_category')
+BEGIN
+    CREATE INDEX [IX_Categories_ProjectId] ON [Categories] ([ProjectId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530142019_add_category')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220530142019_add_category', N'6.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530193522_add_category_to_project')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220530193522_add_category_to_project', N'6.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530221525_project_domain_typo_fixed')
+BEGIN
+    EXEC sp_rename N'[Projects].[CreatedDate]', N'DateCreated', N'COLUMN';
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530221525_project_domain_typo_fixed')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220530221525_project_domain_typo_fixed', N'6.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530223512_category_debug')
+BEGIN
+    ALTER TABLE [Categories] DROP CONSTRAINT [FK_Categories_Projects_ProjectId];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530223512_category_debug')
+BEGIN
+    DROP INDEX [IX_Categories_ProjectId] ON [Categories];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530223512_category_debug')
+BEGIN
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Categories]') AND [c].[name] = N'ProjectId');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Categories] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [Categories] DROP COLUMN [ProjectId];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530223512_category_debug')
+BEGIN
+    ALTER TABLE [Projects] ADD [CategoryId] uniqueidentifier NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000';
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530223512_category_debug')
+BEGIN
+    CREATE INDEX [IX_Projects_CategoryId] ON [Projects] ([CategoryId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530223512_category_debug')
+BEGIN
+    ALTER TABLE [Projects] ADD CONSTRAINT [FK_Projects_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Categories] ([CategoryId]) ON DELETE CASCADE;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530223512_category_debug')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220530223512_category_debug', N'6.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530225902_category_fixed')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220530225902_category_fixed', N'6.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220530230511_deneme123')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220530230511_deneme123', N'6.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
